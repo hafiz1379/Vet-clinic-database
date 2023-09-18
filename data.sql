7a9703ce67f2599e864b5fd6d54349e5e5375523
hafiz1379 <hafizrasa1379@gmail.com>
2023-09-18 16:31:10 +0430

    Add initial animal data to the database

---
+++ data.sql
@@ -0,0 +1,5 @@
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES
+  (1, 'Agumon', '2020-02-03', 0, true, 10.23),
+  (2, 'Gabumon', '2018-11-15', 2, true, 8),
+  (3, 'Pikachu', '2021-01-07', 1, false, 15.04),
+  (4, 'Devimon', '2017-05-12', 5, true, 11);
