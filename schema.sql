60565883c756a274ea06e34dbc2d2493b1564810
hafiz1379 <hafizrasa1379@gmail.com>
2023-09-18 16:30:50 +0430

    Create 'animals' table in the database

---
+++ schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE animals (
+    id INTEGER PRIMARY KEY ,
+    name VARCHAR(50),
+    date_of_birth DATE,
+    escape_attempts INTEGER,
+    neutered BOOLEAN,
+    weight_kg DECIMAL(5,2)
+);
