d0f6150837b7bfbc75d3a441b6ffe4cdc37f6a29
hafiz1379 <hafizrasa1379@gmail.com>
2023-09-19 16:36:15 +0430

    Insert data into the 'animals' table

--- data.sql
+++ data.sql
@@ -4,3 +4,15 @@ VALUES
    ('Gabumon', '2018-11-15', 2, true, 8),
    ('Pikachu', '2021-01-07', 1, false, 15.04),
    ('Devimon', '2017-05-12', 5, true, 11);
+
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg, species)
+VALUES
+    ('Charmander', '2020-02-08', 0, FALSE, -11.0, 'Unspecified'),
+    ('Plantmon', '2021-11-15', 2, TRUE, -5.7, 'Unspecified'),
+    ('Squirtle', '1993-04-02', 3, FALSE, -12.13, 'Unspecified'),
+    ('Angemon', '2005-06-12', 1, TRUE, -45.0, 'Unspecified'),
+    ('Boarmon', '2005-06-07', 7, TRUE, 20.4, 'Unspecified'),
+    ('Blossom', '1998-10-13', 3, TRUE, 17.0, 'Unspecified'),
+    ('Ditto', '2022-05-14', 4, TRUE, 22.0, 'Unspecified');
+    
