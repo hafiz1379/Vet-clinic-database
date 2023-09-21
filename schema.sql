9460ea81e1b23536027b7ecf864101ec53263a66
hafiz1379 <hafizrasa1379@gmail.com>
2023-09-21 10:59:35 +0430

    Add owners & species table. update animals table

--- schema.sql
+++ schema.sql
@@ -4,9 +4,21 @@ CREATE TABLE animals (
     date_of_birth DATE,
     escape_attempts INTEGER,
     neutered BOOLEAN,
-    weight_kg DECIMAL(5,2)
+    weight_kg DECIMAL(5,2),
+    species_id INTEGER REFERENCES species(id),
+    owner_id INTEGER REFERENCES owners(id)
 );
 
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(100),
+    age INTEGER
+);
+
+CREATE TABLE species (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50)
+);
 
 ALTER TABLE animals
 ADD COLUMN species VARCHAR(50);
