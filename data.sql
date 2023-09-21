99442872e51d2334b1d5d75251da55dcb1b4b4eb
hafiz1379 <hafizrasa1379@gmail.com>
2023-09-21 18:45:51 +0430

    Insert data into tables &  establish relationships

--- data.sql
+++ data.sql
@@ -45,3 +45,43 @@ SET
         WHEN name IN ('Angemon', 'Boarmon') THEN (SELECT id FROM owners WHERE full_name = 'Dean Winchester')
         ELSE NULL
     END);
+
+
+-- add 'join table' for visits
+    
+INSERT INTO vets (name, age, date_of_graduation)
+VALUES
+    ('Vet William Tatcher', 45, '2000-04-23'),
+    ('Vet Maisy Smith', 26, '2019-01-17'),
+    ('Vet Stephanie Mendez', 64, '1981-05-04'),
+    ('Vet Jack Harkness', 38, '2008-06-08');
+
+INSERT INTO specializations (vet_id, species_id)
+VALUES
+    (1, 1), -- William Tatcher specialized in Pokemon
+    (3, 2), -- Stephanie Mendez specialized in Digimon
+    (3, 1), -- Stephanie Mendez also specialized in Pokemon
+    (4, 2); -- Jack Harkness specialized in Digimon
+
+INSERT INTO visits (animal_id, vet_id, visit_date)
+VALUES
+    (1, 1, '2020-05-24'), -- Agumon visited William Tatcher
+    (1, 3, '2020-07-22'), -- Agumon also visited Stephanie Mendez
+    (2, 4, '2021-02-02'), -- Gabumon visited Jack Harkness
+    (3, 2, '2020-01-05'), -- Pikachu visited Maisy Smith
+    (3, 2, '2020-03-08'), -- Pikachu visited Maisy Smith again
+    (3, 2, '2020-05-14'), -- Pikachu visited Maisy Smith yet again
+    (4, 3, '2021-05-04'), -- Devimon visited Stephanie Mendez
+    (5, 4, '2021-02-24'), -- Charmander visited Jack Harkness
+    (6, 2, '2019-12-21'), -- Plantmon visited Maisy Smith
+    (6, 1, '2020-08-10'), -- Plantmon also visited William Tatcher
+    (6, 2, '2021-04-07'), -- Plantmon visited Maisy Smith again
+    (7, 3, '2019-09-29'), -- Squirtle visited Stephanie Mendez
+    (8, 4, '2020-10-03'), -- Angemon visited Jack Harkness
+    (8, 4, '2020-11-04'), -- Angemon visited Jack Harkness again
+    (9, 2, '2019-01-24'), -- Boarmon visited Maisy Smith
+    (9, 2, '2019-05-15'), -- Boarmon visited Maisy Smith again
+    (9, 2, '2020-02-27'), -- Boarmon visited Maisy Smith again
+    (9, 2, '2020-08-03'), -- Boarmon visited Maisy Smith again
+    (10, 3, '2020-05-24'), -- Blossom visited Stephanie Mendez
+    (10, 1, '2021-01-11'); -- Blossom also visited William Tatcher
